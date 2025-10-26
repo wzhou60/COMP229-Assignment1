8fb17d66fa770d4e0e6f0ecc4afacb9c1114681c
dillcheese <dillcheese1@gmail.com>
2025-10-26 02:19:31 -0400

    fixed hash password stuff

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -33,8 +33,8 @@ UserSchema.virtual("password")
   .set(function (password) {
     this._password = password;
     this.salt = this.makeSalt();
-    this.hashed_password = password;
-    //this.hashed_password = this.encryptPassword(password);
+    //this.hashed_password = password;
+    this.hashed_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password;
